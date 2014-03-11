d9b3855d9f4f336b13f186afd547620a33201a3b
Andrew Turner <andrew@sq1.com>
2014-03-11 03:37:58 -0500

    added basic deployment recipe

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,39 +1,18 @@
+# Define stage for settings (Capistrano assumes this but better safe than sorry)
+set :stage, :production
+
 # Simple Role Syntax
 # ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-# Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{ubuntu@54.186.89.3}
+# role :web, %w{ubuntu@54.186.89.3}
+# role :db,  %w{deploy@example.com}
 
-# Extended Server Syntax
+# Extended Server Syntax (with SSH Options)
 # ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# settings per server override global ssh_options
+server '54.186.89.3', user: 'ubuntu', roles: %w{web app}
 
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
+# Define deploy_to directory on server 
+# ==================
+# example shows :application and :stage vars but "/var/www/cap-demo/development" works too
+set :deploy_to, "/var/www/#{fetch(:application)}/#{fetch(:stage)}"
