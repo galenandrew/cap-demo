09672fc6187ff153fa886bd3e123661ef4a87b2e
Andrew Turner <andrew@sq1.com>
2014-03-11 13:20:37 -0500

    added value for log_level (verbose output in command line)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,25 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+# Default value for :log_level is :debug
+set :log_level, :info
+
+# Application Details
+# ==================
 set :application, 'cap-demo'
 set :repo_url, 'git@github.com:galenandrew/cap-demo.git'
-
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for keep_releases is 5
-set :keep_releases, 3
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for keep_releases is 5
+set :keep_releases, 3
 
 # Global SSH Options (shared across all stages)
 # ==================
